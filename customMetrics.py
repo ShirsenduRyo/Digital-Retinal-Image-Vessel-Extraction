16fb5ead9fe4e7edde075049bf9dfd45f70e9e88
ShirsenduRyo <sportshirsendu@gmail.com>
2021-10-28 23:51:50 +0530

    Starting from begining

--- customMetrics.py
+++ customMetrics.py
@@ -20,3 +20,12 @@ def dice_coef(y_true, y_pred):
 
 def dice_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
+
+def jaccard_coef(y_true, y_pred):
+    y_true = tf.keras.layers.Flatten()(y_true)
+    y_pred = tf.keras.layers.Flatten()(y_pred)
+    intersection = tf.reduce_sum(y_true * y_pred)
+    return (intersection + 1) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) - intersection + 1)
+
+def jaccard_loss(y_true, y_pred):
+    return - jaccard_coef(y_true, y_pred)
